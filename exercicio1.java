89f764784a19a15e8d50fcd4f0b05950375e631a
Emanuel0101-coder <emanuell.almeida95@gmail.com>
2025-03-07 19:33:12 -0300

    Atualizacao

---
+++ exercicio1.java
@@ -0,0 +1,13 @@
+package ListaExercicio.ListaJava1;
+/*Escreva um programa em java que declare uma vari´avel do tipo inteiro.
+Atribua o maior valor poss´ıvel para essa vari´avel. Some mais 1 e apresente
+o resultado em tela. Observe e analise o resultado*/
+
+public class exercicio1 {
+  public static void main(String args[]){
+    int x = 10;
+    System.out.println(x);
+    System.out.println( ++ x);
+  }
+  
+}
