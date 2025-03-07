7198d8b613a0d41a5f190fbbe3c3ca1fe9b003de
Emanuel0101-coder <emanuell.almeida95@gmail.com>
2025-03-07 19:42:21 -0300

    Atualizando exercicio

--- ListaExercicios/exercicio1.java
+++ ListaExercicios/exercicio1.java
@@ -4,10 +4,12 @@ Atribua o maior valor poss´ıvel para essa vari´avel. Some mais 1 e apresente
 o resultado em tela. Observe e analise o resultado*/
 
 public class exercicio1 {
-  public static void main(String args[]){
-    int x = 10;
-    System.out.println(x);
-    System.out.println( ++ x);
+  public static void main(String[] args) {
+      int maxInt = Integer.MAX_VALUE; // Atribuindo o maior valor possível para um int
+      System.out.println("Maior valor de int: " + maxInt);
+      
+      int resultado = maxInt + 1; // Adicionando 1 ao maior valor possível
+      System.out.println("Resultado após somar 1: " + resultado);
   }
-  
 }
+
